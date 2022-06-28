4c27bbc91309acc7904b43bd3fbc577fd504c39d
Christiaan de Ridder <christiaan@channelengine.com>
2022-06-28 19:41:33 +0200

    Query for blogs

--- WhereInTvp.Console/Program.cs
+++ WhereInTvp.Console/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Microsoft.EntityFrameworkCore;
+using WhereInTvp.Console;
+
+await using var db = new BloggingContext();
+
+var ids = new[] {1, 2};
+
+var blogs = await db.Blogs
+    .Where(b => ids.Contains(b.BlogId))
+    .ToListAsync();
+
+foreach (var blog in blogs)
+{
+    Console.WriteLine(blog.Url);
+}
+
+Console.ReadKey();
