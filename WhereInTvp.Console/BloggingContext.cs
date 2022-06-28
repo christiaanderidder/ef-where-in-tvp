04b47acf7e06aba68303f7d15fd35a63f27f358c
Christiaan de Ridder <christiaan@channelengine.com>
2022-06-28 19:39:37 +0200

    Move classes to separate files

---
+++ WhereInTvp.Console/BloggingContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WhereInTvp.Console;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    public BloggingContext()
+    {
+        
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer($"Server=tcp:localhost,11433;Database=Blogging;Uid=sa;Password=s0mEStr0ngPwd!!");
+}
